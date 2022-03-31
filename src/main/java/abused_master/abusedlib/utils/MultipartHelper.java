9c2569e0cce1745c4a16a33437d95c3f0f68dbb6
PTOM76 <ptms76@outlook.jp>
2022-04-01 01:44:01 +0900

    Port to 1.18.2

--- src/main/java/abused_master/abusedlib/utils/MultipartHelper.java
+++ src/main/java/abused_master/abusedlib/utils/MultipartHelper.java
@@ -1,25 +1,29 @@
 package abused_master.abusedlib.utils;
 
 import abused_master.abusedlib.blocks.multipart.IMultipart;
+import abused_master.abusedlib.mixins.MixinBlockEntity;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 
 public class MultipartHelper {
 
-    public static CompoundTag serialize(IMultipart multipart, CompoundTag tag) {
-        multipart.getMultipartEntity().toTag(tag);
-        tag.putString("multipartIdentifier", Registry.BLOCK_ENTITY.getId(multipart.getMultipartEntity().getType()).toString());
+    public static NbtCompound serialize(IMultipart multipart, NbtCompound tag) {
+
+        ((MixinBlockEntity)multipart.getMultipartEntity()).invoteWriteNbt(tag);
+        tag.putString("multipartIdentifier", Registry.BLOCK_ENTITY_TYPE.getId(multipart.getMultipartEntity().getType()).toString());
 
         return tag;
     }
 
-    public static IMultipart deserialize(CompoundTag tag) {
-        BlockEntity multipartEntity = Registry.BLOCK_ENTITY.get(new Identifier(tag.getString("multipartIdentifier"))).instantiate();
+    public static IMultipart deserialize(NbtCompound tag, BlockPos pos, BlockState state) {
+        BlockEntity multipartEntity = Registry.BLOCK_ENTITY_TYPE.get(new Identifier(tag.getString("multipartIdentifier"))).instantiate(pos, state);
 
         if(multipartEntity != null) {
-            multipartEntity.fromTag(tag);
+            ((MixinBlockEntity)multipartEntity).invokeReadNbt(tag);
         }
 
         return (IMultipart) multipartEntity;
